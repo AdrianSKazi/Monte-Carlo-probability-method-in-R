0f01e99ae57ddccc8005da9593517e4518604a34
ASaati <64697142+AdrianSaati@users.noreply.github.com>
2020-06-02 17:51:34 +0200

    Update Monte Carlo probability.R

--- Monte Carlo probability.R
+++ Monte Carlo probability.R
@@ -1,28 +1,7 @@
-# a - yellow
-# b - green
-# c - purple
-# d - red
-
-genesis <- function(n,a,b,c,d)
-{
-success <- 0 
-bonus <- sample(c('tak','nie'),1)
-
-for (i in 1:n)
-{
-  if (bonus == 'tak')
-  {
-    if (sample(a,1)+sample(b,1) > sample(d,1)) {success = success + 1}
-  }
-  else
-  {
-    if (sample(a,1)+sample(b,1) > sample(c,1)+sample(d,1)) {success = success + 1}
-  }
-  i = i + 1
-}
-return (success / n)
-return (bonus)
-}
+# a - yellow die
+# b - green die
+# c - purple die
+# d - red die
 
 n <- 10000
 a <- c(0,1,1,2,0,0,1,0)
@@ -30,7 +9,17 @@ b <- c(0,1,1,2,2,0,1,1,1,0,0,1)
 c <- c(0,1,2,0,0,0,0,1)
 d <- c(0,1,1,2,2,0,0,1,1,0,0,1)
 
+genesis <- function(n,a,b,c,d)
+{
+success <- 0 
+
+  for (i in 1:n)
+  {
+    if (sample(a,1)+sample(b,1) > sample(c,1)+sample(d,1)) {success = success + 1}
+    i = i + 1
+  }
+
+return (success / n)
+}
+
 genesis(n,a,b,c,d)
-
-
-
